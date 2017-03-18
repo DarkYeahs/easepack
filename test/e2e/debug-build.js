fb2005d208700dd8f3611b5fa529c81e9e2bf314
dante1977 <dante1977@163.com>
2017-03-18 14:38:44 +0800

    debug

--- test/e2e/debug-build.js
+++ test/e2e/debug-build.js
@@ -5,9 +5,12 @@ easeapck({
     {pattern: 'base64/*.png', props: {url: '[name].str.[ext]?[hash]'}},
     {pattern: '*.{js,html}', props: {}}
   ],
+  dev: true,
+  port: 8080,
   useEs2015: true,
   useBase64: '15kb',
   publicPath: '//cc.cdn.com/',
+  config: 'easepack.config.js',
   spriteUrl: '[name].spr.[ext]?[hash]'
 }).run(function (stats) {
   process.stdout.write(stats.toString({
