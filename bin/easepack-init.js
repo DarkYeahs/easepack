e4dab07234fc9e895eee114e7fef4310bb7aa381
dante1977 <dante1977@163.com>
2017-03-26 23:27:37 +0800

    init

--- bin/easepack-init.js
+++ bin/easepack-init.js
@@ -1,19 +1,44 @@
 var os = require('os');
+var fs = require('fs');
 var Path = require('path');
 var program = require('commander');
 var exec = require('child_process').exec;
 var loaderUtils = require("loader-utils");
+
+var config = require('./easepack-config');
 
 var tempDirName = '.easepack-temp';
 var tempPath = Path.join(os.tmpdir(), '..', tempDirName);
 var repo = 'ssh://git@git-cc.nie.netease.com:32200/frontend/';
 
 program
-  .usage('<template-name> [project-name]')
+  .usage('<project-name> [template-name]')
   .option('--offline', 'use cached template')
   .parse(process.argv);
 
-var template = program.args[0];
-var repoUrl = repo + template + '.git';
-var hash = loaderUtils.getHashDigest(repoUrl, null, null, 6);
+var template = program.args[1];
+var repoUrl = repo + (template || 'ep_dome') + '.git';
+var project = Path.join(config.context, program.args[0]);
+var hashUrl = loaderUtils.getHashDigest(repoUrl, null, null, 6);
+var templatePath = Path.join(tempPath, 'template', hashUrl);
 
+fs.access(templatePath, function (err) {
+  var commend = !err ?
+    ['git', 'pull', 'origin'] :
+    ['git', 'clone', repoUrl, templatePath];
+  var options = {cwd: templatePath, stdio: 'inherit'};
+
+  exec(commend.join(' '), options, function (err) {
+    if (err) throw err;
+    copyTo(templatePath, project, function () {
+
+    });
+  });
+});
+
+function copyTo(from, to, callback) {
+  fs.readdir(from, function (err, files) {
+    if (err) return callback(err);
+
+  });
+}
